a6514b15f075d4a00720562aa2abc7455f045c9f
wabemusic <jvwatanabe@gmail.com>
2025-06-13 18:17:30 -0300

    add: Exercício 03

--- exercicios/exercicio03.js
+++ exercicios/exercicio03.js
@@ -1,2 +1,32 @@
 // 3. Palavras Únicas
 // Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for para extrair todas as palavras únicas e exibi-las em um array.
+
+const prompt = require("prompt-sync")();
+
+const frase = prompt("Digite uma palavra: ");
+const palavras = frase.split(" ");
+
+const contagem = {};
+
+for (i = 0; i < palavras.length; i++) {
+  const palavra = palavras[i];
+
+  if (contagem[palavra]) {
+    contagem[palavra]++;
+  } else {
+    contagem[palavra] = 1;
+  }
+}
+
+const unicas = [];
+
+for (let i = 0; i < palavras.length; i++) {
+  const palavra = palavras[i];
+  if (contagem[palavra] === 1) {
+    if (!unicas.includes(palavra)) {
+      unicas.push(palavra);
+    }
+  }
+}
+
+console.log("Palavras Unicas: ", unicas);

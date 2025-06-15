cd03916005cb626542ed4ca3a624cecb64b927f8
wabemusic <jvwatanabe@gmail.com>
2025-06-15 17:59:13 -0300

    add: exercício 4

--- exercicios/exercicio04.js
+++ exercicios/exercicio04.js
@@ -1,3 +1,29 @@
 // 4. Fatorial Recursivo
-// Implemente function fatorial(n) de forma recursiva; trate n < 0 lançando
-// um Error, e n === 0 retornando 1.
+// 1. Implemente function fatorial(n) de forma recursiva; (n * fatorial (n - 1))
+// 2. trate n < 0 lançando um Error; (Se n < 0 deve lançar um erro)
+// 3. e n === 0 retornando.
+
+// Uma função recursiva é uma função que chama a si mesma; e precisa de um caso base (1)  para não rodar infinitamente
+
+const prompt = require("prompt-sync")();
+
+const numero = parseInt(prompt("Digite um número para calcular a fatorial: "));
+
+const fatorial = (n) => {
+  if (n < 0) {
+    throw new Error("Não existe fatorial de número negativo.");
+  }
+
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * fatorial(n - 1);
+};
+
+try {
+  const resultado = fatorial(numero);
+  console.log(`A Fatorial de ${numero} é: ${resultado}`);
+} catch (erro) {
+  console.log(erro.message);
+}

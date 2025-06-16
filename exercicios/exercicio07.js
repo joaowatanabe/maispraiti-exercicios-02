395bb5f91982e1988f28ac391a693f407b912ee1
wabemusic <jvwatanabe@gmail.com>
2025-06-16 09:55:02 -0300

    add: exercício 7

--- exercicios/exercicio07.js
+++ exercicios/exercicio07.js
@@ -2,3 +2,24 @@
 // Dado um array produtos = [{ nome, preco }, …], crie uma função que
 // retorne um novo array apenas com os nomes, ordenados por preço
 // crescente, usando map, sort.
+
+// a funcao deve: 
+// 1. ordenar os produtos com preco crescente (menor > maior) .sort
+// 2. retornar um novo array so com os nomes .map
+
+
+const produtos = [
+  { nome: "teclado", preco: 100 },
+  { nome: "mouse", preco: 50 },
+  { nome: "monitor", preco: 800 },
+  { nome: "macbook", preco: 10000 },
+
+];
+
+const produtosOrdenados = (produtos) => {
+  const ordenado = [...produtos].sort((a, b) => a.preco - b.preco) // ordena o preco
+
+  return ordenado.map(produto => produto.nome) // retorna os nomes
+};
+
+console.log(produtosOrdenados(produtos));

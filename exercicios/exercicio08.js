42fa4e9883f7e79e9475384ef5ad08925774311e
wabemusic <jvwatanabe@gmail.com>
2025-06-16 10:28:03 -0300

    add: exercício 8

--- exercicios/exercicio08.js
+++ exercicios/exercicio08.js
@@ -1,3 +1,28 @@
 // 8. Agrupamento por Propriedade
 // Em vendas = [{ cliente, total }, …], use reduce para gerar um objeto onde
 // cada chave é um cliente e o valor é a soma de todos os seus total.
+
+const vendas = [
+  { cliente: "Ana", total: 100 },
+  { cliente: "Joao", total: 200 },
+  { cliente: "Joao", total: 550 },
+  { cliente: "Ana", total: 300 },
+];
+
+// usar o reduce para gerar um obj onde cada chave é o nome do cliente e o valor é a soma dos totais;
+// { Ana: 400, Joao: 750 }
+
+const agruparVendas = (vendas) => {
+  return vendas.reduce((acc, venda) => {
+    if (acc[venda.cliente]) { //acc vai guardar os totais , venda é cada item do array vendas
+      acc[venda.cliente] += venda.total;
+    } else {
+      acc[venda.cliente] = venda.total;
+    }
+
+    return acc;
+  }, {}); // começa com obj vazio
+};
+
+const resultado = agruparVendas(vendas);
+console.table(resultado);

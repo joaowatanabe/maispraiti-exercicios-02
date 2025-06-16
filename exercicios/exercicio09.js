77681d428dcbd4834961c95253a52748aee35464
wabemusic <jvwatanabe@gmail.com>
2025-06-16 10:51:38 -0300

    add: exercício 9

--- exercicios/exercicio09.js
+++ exercicios/exercicio09.js
@@ -1,7 +1,32 @@
 // 9. Conversão Entre Formatos
 // Escreva duas funções:
-    // - paresParaObjeto(pares) recebe um array de pares [ [chave,
+// - paresParaObjeto(pares) recebe um array de pares [ [chave,
 // valor], … ] e retorna o objeto equivalente.
 
 //     - objetoParaPares(obj) faz o inverso, retornando um array de
 // pares.
+
+// recebe um array de pares, [["nome", "joao"], ["idade", 28]] e retorna um obj equivalente { nome: "joao"}...
+const paresParaObjeto = (pares) => {
+  return pares.reduce((obj, [chave, valor]) => {
+    obj[chave] = valor;
+    return obj;
+  }, {});
+};
+
+// faz o contrario, recebe um obj {nome: "joao"}... e retorna um array [["nome", "joao"], ["idade", 28]]
+const objetoParaPares = (obj) => {
+  return Object.entries(obj);
+};
+
+const pares = [
+  ["nome", "joao"],
+  ["idade", 28],
+];
+
+const obj = paresParaObjeto(pares);
+
+console.log(obj);
+
+const novosPares = objetoParaPares(obj);
+console.log(novosPares);
